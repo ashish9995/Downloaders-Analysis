fb9095fc3425d49960b0a99dda4273b7d3bf3e18
Ashish Pawar <pawarashish@gmail.com>
2018-11-27 02:01:44 +0530

    addition of path and labels

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 ### Set your path to the folder containing the .csv files
-PATH = 'C:\Users\ashis\Downloads\connection_speed\connection_speed\data' # Use your path
+PATH = 'C:\\Users\\ashis\\Downloads\\connection_speed\\connection_speed\\data' # Use your path
 
 ### Fetch all files in path
 fileNames = os.listdir(PATH)
@@ -15,10 +15,11 @@ fileNames = [file for file in fileNames if '.csv' in file]
 for file in fileNames:
 
     ### Read .csv file and append to list
-    df = pd.read_csv(PATH + file, index_col = 0)
+    df = pd.read_csv(PATH + file, index_col = None)
 
     ### Create line for every file
-    plt.plot(df)
+    plt.plot(df,label=file)
+    plt.legend()
 
 ### Generate the plot
 plt.show()
